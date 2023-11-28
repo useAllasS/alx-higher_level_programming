699c63a70170f13322de0543b26de3a964d44d11
Nita Youssef <nitayoussef3@gmail.com>
2023-11-28 10:18:50 -0800

    ff

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 
 # Calculate the last digit of the number
 last_digit = abs(number) % 10
-
-# Generate the output string
+number=number
+#Generate the output string
 output = "Last digit of {} is {}".format(number, last_digit)
 
 # Check the conditions and append the appropriate string
