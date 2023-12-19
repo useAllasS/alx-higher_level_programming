c6de9b38c9b37dbe2c27cbda5aa14e7886957ece
Nita Youssef <nitayoussef3@gmail.com>
2023-12-19 00:14:29 -0800

    dij

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-    while idx < x:
-        try:
-            print("{}".format(my_list[idx]), end="")
-        except IndexError:
-            break
-        idx += 1
-    print("")
-    return idx
+    i = 0
+    if x == 0:
+        print()
+        return i
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+    except IndexError:
+        print()
+        return i
+    return i + 1
